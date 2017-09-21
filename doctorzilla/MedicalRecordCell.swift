86b84e2d4e6168cb914daf8c5f20ef3b84582b7b
jmerchan <merchan1395@gmail.com>
2017-09-21 01:07:42 -0400

    New Layout

---
+++ doctorzilla/MedicalRecordCell.swift
@@ -0,0 +1,24 @@
+//
+//  MedicalRecordCell.swift
+//  doctorzilla
+//
+//  Created by Javier Merchán on 9/20/17.
+//  Copyright © 2017 Merchan. All rights reserved.
+//
+
+import UIKit
+
+class MedicalRecordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
